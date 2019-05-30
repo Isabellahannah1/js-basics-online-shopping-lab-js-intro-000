f7d7e8dd7a45fa9b1470ae33f63f0707030b7dba
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-30 12:13:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,20 +10,68 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemObj = {itemName:`${item}`, itemPrice: Math.floor(Math.random()*100)}; // this is how to randomly assign an integer
+  // console.log(itemObj);
+  cart.push(itemObj);
+  return itemObj.itemName + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){ 
+    return "Your shopping cart is empty.";
+  }
+  else if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  else if (cart.length === 2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }
+  else if (cart.length >= 3){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`;
+  }
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  for(var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  delete itemPrice// write your code here
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
+
+describe("removeFromCart()", function() {
+  it("removes the specified item from the cart", function() {
+    addToCart("vanilla");
+    addToCart("watermelon");
+    addToCart("yams");
+
+    removeFromCart("watermelon");
+
+    const firstItemName = getCart()[0].itemName;
+    const secondItemName = getCart()[1].itemName;
+
+    expect(firstItemName).toEqual("vanilla");
+    expect(secondItemName).toEqual("yams");
+
+    removeFromCart("yams");
+
+    expect(getCart().length).toEqual(1);
+  });
+
+  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
+    // Repeat item name from previous test to prevent hard-coding.
+    expect(removeFromCart("yams")).toEqual("That item is not in your cart.");
+  });
+});
 
 function placeOrder(cardNumber) {
   // write your code here
